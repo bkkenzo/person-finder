72e00ae4e5b6a84e5fe13368d7410282c7a07bf4
Nayyif Oussamatou <aremoustar@gmail.com>
2020-08-07 11:42:28 -0400

    first commit

---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: __dirname,
+    // publicPath: '/',
+    filename: './public/bundle.js'
+  },
+  devServer: {
+    contentBase: './public',
+  },
+  module: {
+    rules: [
+    {
+      test: /\.(js|jsx)$/,
+      exclude: /node_modules/,
+      use: ['babel-loader']
+    },
+    {
+      test: /\.less$/,
+      use: [
+        'style-loader',
+        'css-loader',
+        'less-loader',
+      ],
+    }
+    ]
+  },
+};
